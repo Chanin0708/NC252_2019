3925d63ad84a7768968df1b76d4b06fcd81d4fc0
Chanin0708 <54060999+Chanin0708@users.noreply.github.com>
2019-08-13 16:21:25 +0700

    Add files via upload

---
+++ RegistrationRecord.java
@@ -0,0 +1,6 @@
+package th.ac.utcc.cpe.nc252.Chanin;
+
+public class RegistrationRecord {
+	private Student Student;
+	private Subject [] Subject;
+}
---
+++ Student.java
@@ -0,0 +1,37 @@
+package th.ac.utcc.cpe.nc252.Chanin;
+
+public class Student {
+
+	private String Name;
+	private String id ;
+	private String tel;
+	private String Email;
+	
+	
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	
+	
+}
